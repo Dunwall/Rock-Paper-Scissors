8b1a92faa20dfcf5acb75eb6e005c3ddf27cc036
Dunwall <hynniewtajordan@gmail.noreply.github.com>
2025-07-12 14:01:10 +0000

    Added a UI to the RockPaperScissors gmae

--- RPS.js
+++ RPS.js
@@ -1,102 +1,129 @@
-// 1. CREATE COMPUTER FUNCTION THAT GENERATES RANDOM VALUE IN RESPONSE
-// TO USER
-// 2. CREATE PLAYER FUNCTION THAT RETURNS PLAYER'S INPUT
-// 3. CRAFT RESPONSES ACCORDING TO THE GAME'S RULES
-// 4. INCREMENT AFTER EACH PLAY, JUST OUTPUT FINAL SCORE FOR NOW
-// 5. IMPORTANT! MAKE A LOOP SO USER CAN PLAY 5 TIMES
-
-// VARIABLES FOR SCORES
 let humanScore=0;
 let computerScore=0;
-// FUNCTION TO GET RANDOM NUMBER FOR COMPUTER
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
+const final = document.querySelector('#final');
+const firstImage = document.querySelector('.firstImage');
+const secondImage = document.querySelector('.secondImage');
+const thirdImage = document.querySelector('.thirdImage');
+
+const arr = ["images/rock.jpg","images/paper.jpg","images/scissors.jpg"];
+
+function appendImg(div,source){
+    div.innerHTML = '';
+    const img = document.createElement('img');
+    img.src = source;
+    div.appendChild(img);
+}   
+
+// 
 function getRandomInt(max){
-    return Math.floor(Math.random()*max);
+return Math.floor(Math.random()*max);
 }
 
-// COMPUTER OUTPUTS CHOICE, 0,1,2=>Rock, Paper, Scissors
+const div = document.createElement('div');
 function getComputerChoice(){
     const choice=getRandomInt(3);
     if(choice===0){
-        console.log('ROCK');
+        logResult('Computer played Rock');
+        appendImg(thirdImage,arr[0]);
     }
     else if(choice===1){
-        console.log('PAPER');
+        logResult('Computer Played Paper');
+        appendImg(thirdImage,arr[1]);
     }
     else{
-        console.log('SCISSORS');
+        logResult('Computer Played Scissors');
+        appendImg(thirdImage,arr[2]);
     }
     return choice;
 }
-
-// HUMAN ENTERS CHOICE, NO ERROR CHECKING YET
-function getHumanChoice(){
-    let humanChoice=prompt("Please Enter Your Choice: ");
-    let normalized=humanChoice.toLowerCase();
-    console.log(normalized);
-    if(humanChoice=='rock'){
-        return 0;
-    }
-    else if(humanChoice=='paper'){
-        return 1;
-    }
-    else if(humanChoice=='scissors'){
-        return 2;
-    }
-    else{
-        prompt("Invalid input!");
-        return getHumanChoice();
-    }
-}
-
-// EACH ROUND WHERE HUMAN PLAYS AND COMPUTER PLAYS AND OUTPUTS
-// A MESSAGE IF DRAW OR NOT
 function playRound(humanSelection,computerSelection){
     if(humanSelection==1 && computerSelection==1 ||
         humanSelection==0 && computerSelection==0 ||
         humanSelection==2 && computerSelection==2
     ){
-        console.log("It's a Draw!");
-        alert('Draw');
+        logResult('Its a draw');
     }
     else if(humanSelection==1 && computerSelection==0){
-        console.log("Paper Beats Rock!");
-        alert('human won with paper');
+        logResult('Human won with paper');
         humanScore++;
     }
     else if(humanSelection==2 && computerSelection==1){
-        console.log("Scissors Beats Paper!");
-        alert('human won with scissors');
+        logResult('Human won with scissors');
         humanScore++;
     }
     else if(humanSelection==0 && computerSelection==2){
-        console.log("Rock Beats Scissors");
-        alert('human won with rock');
+        logResult('Human won with rock');
         humanScore++;
     }
     else if(computerSelection==0 && humanSelection==2){
-        console.log("Rock Beats Scissors!");
-        alert('Computer won with rock');
+        logResult('Computer won with Rock');
         computerScore++;
     }
     else if(computerSelection==1 && humanSelection==0){
-        console.log("Paper Beats Rock!");
-        alert('Computer won with paper');
+        logResult('Computer won with paper');
         computerScore++;
     }
     else if(computerSelection==2 && humanSelection==1){
-        console.log("Scissors Beats Paper!");
-        alert('Computer won with scissors');
+        logResult('Computer won with scissors')
         computerScore++;
     }
 }
 
-function playGame(){
-    // CALLING EACH FUNCTION
-    const humanSelection=getHumanChoice();
-    const computerSelection=getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    alert(`Human: ${humanScore} , Computer: ${computerScore}`);
+function removeTransition(e){
+    if(e.propertyName !== 'box-shadow')return;
+    e.target.classList.remove('highlight');
 }
-for(let i=0;i<5;i++){
-playGame();
+
+function reset(){
+    final.textContent = ' ';
+    score.textContent = `Human: 0, Computer: 0`;
+    humanScore = 0;
+    computerScore = 0;
+    results.innerHTML = ' ';
+}
+
+function logResult(text){
+    results.textContent = text;
+}
+
+rock.addEventListener('click',()=>{
+    playGame(0);
+    rock.classList.add('highlight');
+    appendImg(firstImage,arr[0]);
+});
+rock.addEventListener('transitionend',removeTransition);
+
+paper.addEventListener('click',()=>{
+    playGame(1);
+    paper.classList.add('highlight');
+    appendImg(firstImage,arr[1]);
+});
+paper.addEventListener('transitionend',removeTransition);
+
+scissors.addEventListener('click',()=>{
+    playGame(2);
+    scissors.classList.add('highlight');
+    appendImg(firstImage,arr[2]);
+});
+scissors.addEventListener('transitionend',removeTransition);
+
+function playGame(humanSelection){
+// CALLING EACH FUNCTION
+const computerSelection=getComputerChoice();
+playRound(humanSelection,computerSelection);
+score.textContent= `Human: ${humanScore} , Computer: ${computerScore}`;
+if(humanScore==5){
+    final.textContent = 'You won!';
+    setTimeout(reset,500);
+}
+if(computerScore==5){
+    final.textContent = 'Computer Won!';
+    setTimeout(reset,500);
+}
 }
